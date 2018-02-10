f13d1ef55c94083695cc08cfa22caaf17fd68344
Hendi Santika <hendisantika@gmail.com>
2018-02-10 21:46:06 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootdockerswagger/SpringbootDockerSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootdockerswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerSwaggerApplication.class, args);
+	}
+}
